0a584aeb2e58a275ae27a516b9d1331be4fbbf84
LiFengONE <614109926@qq.com>
2018-06-14 17:17:27 +0800

    update tools

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/football-dragram/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
